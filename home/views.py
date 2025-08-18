102006594c0a73777e1fb1eb3ab9f9512f3d796b
Anjali Kumari Sahu <160297968+Anjali-Kumari-Sahu@users.noreply.github.com>
2025-08-18 22:28:08 +0530

    33839

--- home/views.py
+++ home/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.conf import settings
+from restaurant_management.models import restaurant_management
+
+def homepage(request):
+    restaurant = Restaurant.objects.first()
+
+    if restaurant:
+        restaurant_name = restaurant.name
+    else:
+        restaurant_name = settings.restaurant_name
+    
+    context = {"restaurant_name": restaurant_nmae}
+    return render(request, "home/index.html", context)
