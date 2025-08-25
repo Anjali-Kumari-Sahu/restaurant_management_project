9b220f967c23889040d9357c71b551f763af47ab
Anjali Kumari Sahu <160297968+Anjali-Kumari-Sahu@users.noreply.github.com>
2025-08-25 23:23:47 +0530

    38129

---
+++ restaurant/view.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def menu_list(request):
+    menu_items = [
+        {"id":1, "name": "Margherita pizza","price": 250},
+        {"id":2, "name": "Paneer Butter Masala" ,"price": 300},
+        {"id":3, "name": "Veg Biriyani " ,"price":200},
+        {"id":4, "name": " Masala Dosa","price" : 150},
+        {"id":5, "name":  "  Cold Coffe ","price":120},
+    ]
+
+    return render(request, "menu.html", {"items": menu_items})
