0e7ea3d6a113dd7fea89ac77f6d60b8ad93e0639
Anjali Kumari Sahu <160297968+Anjali-Kumari-Sahu@users.noreply.github.com>
2025-08-28 01:06:11 +0530

    38131

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import restaurant_about
+from .import views
 
 urlpatterns = [
     path("about\", restaurant_about, name="restaurant_about),
+    path("contact/",  views.contact, name="contact"),
+    
 ]
