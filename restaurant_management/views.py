28ed144fb7eb99e274fdc19bf80ccf552e583090
Anjali Kumari Sahu <160297968+Anjali-Kumari-Sahu@users.noreply.github.com>
2025-08-28 21:41:34 +0530

    40239

--- restaurant_management/views.py
+++ restaurant_management/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from django.db import DatabaseError
+from .models import Restaurant 
  
 def restaurant_about(request):
+    try:
+        restaurant = Restaurant.objects.first()
+
+
+        if restaurant:
+            restaurant_name = restaurant.restaurant_name
+            restaurant_address = restaurant.restaurant_address
+            restaurant_phone = restaurant.restaurant_phone
+        else:
+            restaurant_name = "Foodie's Paradise"
+            restaurant_address = "Address not available"
+            restaurant_phone = "Phone not available"
+
+    except DatabaseError:
+
+        restaurant_name = "Foodie's Paradise"
+        restaurant_address = "Error: could not fetch address"
+        restaurant_phone = "Error: Could not fetch phone"
     
-    return render(request, "restaurant_management/about.html")
+
+
+    return render(request, "restaurant_management/about.html", {
+        "restaurant_name": restaurant_name,
+        "restaurant_address": restaurant_address,
+        "restaurant_phone": restaurant_phone,
+    })
