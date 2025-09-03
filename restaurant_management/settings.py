a1e63874d95983f2d7374acfa0f023269c55b9d7
Anjali Kumari Sahu <160297968+Anjali-Kumari-Sahu@users.noreply.github.com>
2025-09-03 14:04:49 +0530

    42360

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -124,6 +124,38 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+'context_processors': [
+     "django.template.context_processors.debug",
+     "django.template.context_processors.request",
+     "django.contrib.auth.context_processors.auth",
+     "django.contrib.messages.context_processors.messages",
+     "django.template.context_processors.static",
+     "django.template.context_processors.tz",
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
