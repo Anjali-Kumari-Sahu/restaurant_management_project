cdb06068d30ca288060ecf8ba73d0ba41327f3b4
Anjali Kumari Sahu <160297968+Anjali-Kumari-Sahu@users.noreply.github.com>
2025-09-03 14:40:14 +0530

    42358

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -1,1 +1,14 @@
-from django 
+from django import forms
+from .models import Feedback
+
+class FeedbackForms(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['comment']
+        widgets = {
+            'comment': forms.Textarea(attrs={
+                'placeholder': 'write your feedback here....',
+                'route': 4,
+                'cols': 40,
+            }),
+        }
